83f17e21193f636ac29cdbfbb44b52ef7fcf8ac3
Amanda <amawai@github.com>
2018-01-28 00:42:05 -0500

    camera capabilities

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
-
+import DrawerButton from '../Components/DrawerButton.js'
 import { Images } from '../Themes'
-
+import { Button } from 'react-native'
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
   render () {
-    return (
+    const { navigate } = this.props.navigation;
+    return ( 
       <View style={styles.mainContainer}>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
@@ -19,9 +20,10 @@ export default class LaunchScreen extends Component {
 
           <View style={styles.section} >
             <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
+              <Button
+                title='Snap a pic'
+                onPress={() => navigate('CatCamera')}
+              />
           </View>
 
           <DevscreensButton />
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,11 +1,12 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
-
+import CatCamera from '../Containers/CatCamera'
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  CatCamera: { screen: CatCamera }
 }, {
   // Default config for all screens
   headerMode: 'none',
